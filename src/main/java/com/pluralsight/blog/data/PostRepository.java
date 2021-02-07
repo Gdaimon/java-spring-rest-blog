5716390f23f3cd27cf59ca13feb5d79ec7170d45
Carlos Andres <darkklitos@gmail.com>
2021-02-07 11:15:05 -0500

    remove findByTitleContaining

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,27 +1,23 @@
 package com.pluralsight.blog.data;
 
 import com.pluralsight.blog.model.Post;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestResource ( rel = "contains-title", path = "containsTitle" )
 public interface PostRepository extends JpaRepository < Post, Long > {
-	
-	@Autowired
-	DatabaseLoader databaseLoader;
-	
-	default List < Post > findByTitleContaining ( String title ) {
-		List < Post > postList = new ArrayList <> ( );
-		for ( Post post : databaseLoader.randomPosts ) {
-			if ( post.getTitle ( ).contains ( title ) ) {
-				postList.add ( post );
-			}
-		}
-		return postList;
-	}
-	
+
+//	@Autowired
+//	DatabaseLoader databaseLoader= new DatabaseLoader (  );
+//
+//	default List < Post > findByTitleContaining ( String title ) {
+//		List < Post > postList = new ArrayList <> ( );
+//		for ( Post post : databaseLoader.randomPosts ) {
+//			if ( post.getTitle ( ).contains ( title ) ) {
+//				postList.add ( post );
+//			}
+//		}
+//		return postList;
+//	}
+
 }
