10249d0cd589be828771e8571f887ed802d02d26
Carlos Andres <darkklitos@gmail.com>
2021-02-07 11:55:09 -0500

    fix AuthorRepository and ignode Author field

--- src/main/java/com/pluralsight/blog/data/AuthorRepository.java
+++ src/main/java/com/pluralsight/blog/data/AuthorRepository.java
@@ -2,9 +2,10 @@ package com.pluralsight.blog.data;
 
 import com.pluralsight.blog.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@Component
+// nos sirve para no exponene o mostrar los dtaos del author
+@RepositoryRestResource ( exported = false )
 public interface AuthorRepository extends JpaRepository < Author, Long > {
 
 }
