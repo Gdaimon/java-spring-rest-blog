ea71fd4e5f35fd9437f6109f33f1d8d9929b060d
Carlos Andres <darkklitos@gmail.com>
2021-02-07 11:22:08 -0500

    add filter

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -4,20 +4,12 @@ import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-@RestResource ( rel = "contains-title", path = "containsTitle" )
+import java.util.List;
+
+
 public interface PostRepository extends JpaRepository < Post, Long > {
-
-//	@Autowired
-//	DatabaseLoader databaseLoader= new DatabaseLoader (  );
-//
-//	default List < Post > findByTitleContaining ( String title ) {
-//		List < Post > postList = new ArrayList <> ( );
-//		for ( Post post : databaseLoader.randomPosts ) {
-//			if ( post.getTitle ( ).contains ( title ) ) {
-//				postList.add ( post );
-//			}
-//		}
-//		return postList;
-//	}
-
+	
+	@RestResource ( rel = "contains-title", path = "containsTitle" )
+	public List < Post > findByTitleContaining ( String title );
+	
 }
